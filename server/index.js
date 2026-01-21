c12c10141f7b24fd12c937c0e0f64b181577cfa5
Atulx21 <atulparmar0021@gmail.com>
2026-01-21 14:59:42 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+// server/index.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve frontend static files
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Basic connection test
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+    
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
